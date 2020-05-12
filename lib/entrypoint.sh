fe32855410ff87ba7ae2a77b24799d39ecd3a7a0
Gustav Persson <gustav@ustwo.com>
2020-05-12 15:35:44 +0200

    fixes fixes

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-npm install
+cd "${2:-.}" || echo "source root not found"
 
-NODE_PATH=node_modules node /action/lib/run.js
+[ -f package-lock.json ] && npm install
+
+NODE_PATH=node_modules GITHUB_TOKEN=$1 SOURCE_ROOT=${2:-.} node /action/lib/run.js
+
+rm -rf node_modules # cleanup to prevent some wired permission errors later on
